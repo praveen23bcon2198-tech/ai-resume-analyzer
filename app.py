da0d46dc4c4a2867167399a8dca25db1297fb9af
Praveen Kumar <praveen123@gmail.com>
2026-05-17 22:58:37 +0530

    project files

---
+++ app.py
@@ -0,0 +1,82 @@
+from flask import Flask, render_template, request
+import os
+import PyPDF2
+import docx
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+skills = [
+    "Python", "Java", "C++", "HTML", "CSS",
+    "JavaScript", "SQL", "Flask", "Machine Learning"
+]
+
+def extract_text_from_pdf(file_path):
+    text = ""
+    with open(file_path, "rb") as file:
+        reader = PyPDF2.PdfReader(file)
+        for page in reader.pages:
+            extracted = page.extract_text()
+            if extracted:
+                text += extracted
+    return text
+
+def extract_text_from_docx(file_path):
+    doc = docx.Document(file_path)
+    text = ""
+    for para in doc.paragraphs:
+        text += para.text + "\n"
+    return text
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/analyze', methods=['POST'])
+def analyze():
+    file = request.files['resume']
+
+    if file:
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(filepath)
+
+        if file.filename.endswith('.pdf'):
+            text = extract_text_from_pdf(filepath)
+        elif file.filename.endswith('.docx'):
+            text = extract_text_from_docx(filepath)
+        else:
+            text = ""
+
+        found_skills = []
+        missing_skills = []
+
+        for skill in skills:
+            if skill.lower() in text.lower():
+                found_skills.append(skill)
+            else:
+                missing_skills.append(skill)
+
+        total_skills = len(skills)
+        matched_skills = len(found_skills)
+        score = int((matched_skills / total_skills) * 100)
+
+        suggestion = ""
+        if score >= 80:
+          suggestion = "Excellent resume. Your resume is ATS friendly."
+        elif score >= 50:
+         suggestion = "Good resume, but you can add more relevant skills."
+        else:
+         suggestion = "Your resume needs improvement. Add more technical skills."        
+
+        return render_template(
+            'result.html',
+            found_skills=found_skills,
+            missing_skills=missing_skills,
+            score=score,
+            suggestion=suggestion
+        )
+
+if __name__ == '__main__':
+    os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+    app.run(debug=True)
